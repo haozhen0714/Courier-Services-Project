4f52c796644edbbb59ddaf3125418951480e0aaf
Hao Zhen Moy <haozhen0714@gmail.com>
2021-04-23 19:26:26 +0800

    geocoding

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-import SecondClass
+import requests
 
-a = SecondClass.Hello()
-a.print_message()
-print("Hello")
-print("Hello")
+API_KEY = 'AIzaSyDKQY-dAMpv32uiWSREDRH83FZRcNUhSmw'
+
+
+def getGeoCoord(address):
+    params = {
+        'key': API_KEY,
+        'address': address.replace(' ', '+')
+    }
+
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    print(data)
+    if data['status'] == 'OK':
+        result = data['results'][0]
+        location = result['geometry']['location']
+        return location['lat'], location['lng']
+    else:
+        return None
+
+
+print(getGeoCoord(""))
