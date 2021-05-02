715054c9329e8a6e0febafecd4c7dc9598dd68f4
Hao Zhen Moy <haozhen0714@gmail.com>
2021-05-02 12:29:46 +0800

    Create Company Details list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from GeocodingAPI import GeocodingAPIClass
 from Company import CompanyClass
+import json
 
-print(GeocodingAPIClass.getGeoCoord("G-3A,Putra One Residence, Persiaran Bukit Rahman Putra 1, 40160 Sungai Buloh, Selangor"))
+
 
