fbd4ac01c684f5616072782d8df75ffc89d932d5
Hao Zhen Moy <haozhen0714@gmail.com>
2021-05-04 23:14:03 +0800

    Change Customer () ranking file name to Customer () Problem 1 Ranking

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import GeocodingAPI
 from Company import CompanyClass
 from Customer import CustomerClass
 from os import path
-import time
 
 
 def read_raw_company_file():
@@ -40,7 +39,7 @@ def read_raw_customer_file():
     return customer_object_list
 
 
-def calculate_and_write_location_coordinate(company_lists):
+def calculate_and_write_company_location_coordinate(company_lists):
     for company in company_lists:
         location = GeocodingAPI.getGeoCoord(company.address)
         company.latitude = location[0]
@@ -75,31 +74,40 @@ def calc_total_distance(customer, company):
         Company.formatted_coordinates(company.latitude, company.longitude),
         Company.formatted_coordinates(customer.des_lat, customer.des_long))
     total_distance = float(distance1.replace(" km", "")) + float(distance2.replace(" km", ""))
-    return total_distance
+    return (distance1, distance2, total_distance)
 
 
 def generate_file_for_customer_with_each_company(customer_lists, company_lists):
     for customer in customer_lists:
-        customer_file = open(customer.customer_name + ".txt", 'w')
+        customer_file_ranking = open(customer.customer_name + " Problem 1 Ranking.txt", 'w')
+        customer_file_details_route = open(customer.customer_name + " Delivery Details.txt", 'w')
         company_ranking_list = []
         for company in company_lists:
-            total_distance = calc_total_distance(customer, company)
+            return_distances = calc_total_distance(customer, company)
+            customer_file_details_route.write(str(customer.ori_name) + " --> " + str(company.name) + "\n")
+            customer_file_details_route.write(str(return_distances[0]) + "\n")
+            customer_file_details_route.write(str(company.name) + " --> " + str(customer.des_name) + "\n")
+            customer_file_details_route.write(str(return_distances[1]) + "\n\n")
+            total_distance = return_distances[2]
             company_ranking_list.append([company, total_distance])
         company_ranking_list.sort(key=lambda x: x[1])
         count = 0
         for i in range(len(company_ranking_list)):
-            customer_file.write(str(count) + "\n")
-            customer_file.write(str(company_ranking_list[i][0].name) + "\n")
-            customer_file.write(str(company_ranking_list[i][1]) + " km" + "\n\n")
+            customer_file_ranking.write(str(count) + "\n")
+            customer_file_ranking.write(str(company_ranking_list[i][0].name) + "\n")  # company object
+            customer_file_ranking.write(
+                str(customer.ori_name) + " --> " + str(company_ranking_list[i][0].name) + " --> " + str(customer.des_name) + "\n")
+            customer_file_ranking.write(str(company_ranking_list[i][1]) + " km" + "\n\n")  # distance
             if i != len(company_ranking_list) - 1:
                 if company_ranking_list[i][1] != company_ranking_list[i + 1][1]:
                     count += 1
-        customer_file.close()
+        customer_file_ranking.close()
+        customer_file_details_route.close()
 
 
 def preprocessing_company():
     company_list = read_raw_company_file()
-    calculate_and_write_location_coordinate(company_list)
+    calculate_and_write_company_location_coordinate(company_list)
 
 
 def preprocessing_customer():
@@ -108,8 +116,7 @@ def preprocessing_customer():
 
 
 def read_company_full_details():
-    if path.exists("Company_Full_Details.txt") is False:
-        preprocessing_company()
+    preprocessing_company()
     company_file = open("Company_Full_Details.txt", "r")
     company_all_list = company_file.readlines()
     company_object_list = []
@@ -126,8 +133,7 @@ def read_company_full_details():
 
 
 def read_customer_full_details():
-    if path.exists("Customer_Origin_Destination_Including_Distance.txt") is False:
-        preprocessing_customer()
+    preprocessing_customer()
     customer_file = open("Customer_Origin_Destination_Including_Distance.txt", "r")
     customer_all_list = customer_file.readlines()
     customer_object_list = []
@@ -145,5 +151,5 @@ def read_customer_full_details():
 
 company_list = read_company_full_details()
 customer_list = read_customer_full_details()
-generate_file_for_customer_with_each_company(customer_list, company_list)  # generate score for problem 1 and save it in file
-
+generate_file_for_customer_with_each_company(customer_list,
+                                             company_list)  # generate score for problem 1 and save it in file
