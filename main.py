77157d7ce2273b93b5a9cb4c8112c0c9bcf60031
Jing-hui <jinghui25@ymail.com>
2021-06-12 04:03:56 +0800

    updated Problem 1.4 and added Problem 4

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import Problem1 as p1
 import Problem2 as p2
 import Problem3 as p3
-import MapForOneCustomer
+import Problem4 as p4
 import CompanyMap
+import CustomerMap
 import time
 
 initial_time = time.time()
@@ -19,13 +20,10 @@ p1.generate_file_for_customer_with_each_company(customer_list,
 # added for naim code
 CompanyMap.draw_company_map(company_list)
 
-# added by jinghui
-customers_map = []
-for i in range(1, len(customer_list) + 1):
-    customers_map.append(p1.read_customer_ranking_details(str(i)))
-
-for i in range(len(customers_map)):
-    MapForOneCustomer.plotMap(customers_map[i], i + 1)
+# plot maps for each customer, containing all routes origin-hub-destination
+# Time complexity: O(mn), m = no of customer, n = no of company
+for customer in customer_list:
+    CustomerMap.plot_map(customer, company_list)
 
 # P2
 print('----------------------------------------------------')
@@ -42,4 +40,16 @@ p1p2_dict = p3.read_p1_p2_ranking_file(customer_list)
 p3.calc_probability_distribution(p1p2_dict)
 p3.write_final_ranking_file(p1p2_dict)
 
-print("Execution time: %.2f seconds" % (time.time() - initial_time))
+# P4
+print('----------------------------------------------------')
+print("Executing Problem 4")
+# given x = length of series x, y = length of series y
+# Time complexity: (x^2) if x==y
+# due to every cell in the cost matrix is filled to ensure optimal answer is obtained
+p4.display_simple_dtw()
+# given x = length of series x, y = length of series y
+# Time complexity: O(x)
+# due to multilevel approach
+p4.speech_recognition()
+
+print("\nExecution time: %.2f seconds" % (time.time() - initial_time))
